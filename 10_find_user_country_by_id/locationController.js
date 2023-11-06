4261a01d07106e59b6a5e25f3c9baa13c4856c1a
dimaviv <dimaviv1812@gmail.com>
2023-11-06 18:09:21 +0200

    refarctor: implement binary search

--- 10_find_user_country_by_id/locationController.js
+++ 10_find_user_country_by_id/locationController.js
@@ -32,22 +32,31 @@ class LocationController {
             const userIP = req.ip;
 
             if (!userIP) return next(ApiError.badRequest("No IP address detected"))
+            const userIPInt = ipToInt(userIP);
 
             let userLocation = 'Unknown';
             let ipFrom;
             let ipTo;
 
-            for (let row of ipToLocation) {
-                ipFrom = parseInt(row.ipFrom);
-                ipTo = parseInt(row.ipTo);
+            let left = 0;
+            let right = ipToLocation.length - 1;
 
-                const userIPInt = ipToInt(userIP);
+            while (left <= right) {
+                let mid = Math.floor((left + right) / 2);
+
+                ipFrom = ipToLocation[mid].ipFrom;
+                ipTo = ipToLocation[mid].ipTo;
 
                 if (userIPInt >= ipFrom && userIPInt <= ipTo) {
-                    userLocation = csvValueToStr(row.country);
+                    userLocation = csvValueToStr(ipToLocation[mid].country);
                     break;
+                } else if (userIPInt < ipFrom) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
                 }
             }
+
             const ipRange = intIpToHRF(ipFrom) + ' - ' + intIpToHRF(ipTo);
 
             res.json({ ipRange, country: userLocation });
