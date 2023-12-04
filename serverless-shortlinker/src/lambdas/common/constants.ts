601b60bf5d18dffe533d0072cc77ebc635b79937
dimaviv <dimaviv1812@gmail.com>
2023-12-04 16:37:55 +0200

    feat: link expiration time option

---
+++ serverless-shortlinker/src/lambdas/common/constants.ts
@@ -0,0 +1,5 @@
+export const allowedExpireTimeInMinutes = {
+    '1d': 24 * 60,
+    '3d': 3 * 24 * 60,
+    '7d': 7 * 24 * 60,
+}
--- serverless-shortlinker/src/lambdas/links/shortenLink.ts
+++ serverless-shortlinker/src/lambdas/links/shortenLink.ts
@@ -3,6 +3,7 @@ import {Dynamo} from "../common/Dynamo";
 import {verifyToken} from "../common/utils/jwt-utils";
 import {scheduleDeactivation} from "@libs/task-scheduler";
 import {Responses} from "../common/API_Responses";
+import {allowedExpireTimeInMinutes} from "../common/constants";
 
 
 export const shortenLink = async (event) => {
@@ -12,17 +13,22 @@ export const shortenLink = async (event) => {
 
     const id = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 6)();
 
-    const newLink = {id, url, userId: user.sub, createdAt: new Date().toISOString(), expire, isActive:true, visitCount:0}
+    let expireAt = null;
+    if (expire && expire !== 'once'){
+        const min = allowedExpireTimeInMinutes[expire]
+        expireAt = new Date(new Date().getTime() + min * 60000).toISOString();
+    }
+
+    const newLink = {id, url, userId: user.sub, createdAt: new Date().toISOString(), expire, expireAt, isActive:true, visitCount:0}
 
     const link = await Dynamo.write(newLink, process.env.LINKS_TABLE)
     console.log('link', link)
     const shortLink = `${process.env.HOST_URL}/${link.id}`;
 
-    //set invocation time
-    const time = new Date(new Date().getTime()).toISOString();
-
-    const schedule = await scheduleDeactivation(link.id, time, {id:link.id});
-    console.log(schedule)
+    if (expireAt){
+        const schedule = await scheduleDeactivation(link.id, expireAt, {id:link.id});
+        console.log(schedule)
+    }
 
     return Responses._200({ shortLink })
 };
